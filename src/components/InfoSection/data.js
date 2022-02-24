9c6baae04c334fdecfcbc245a252cb5a96b80579
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-02-25 06:08:17 +0630

    info section

---
+++ src/components/InfoSection/data.js
@@ -0,0 +1,55 @@
+import img1  from '../../images/svg-1.svg';
+import img2 from '../../images/svg-2.svg'
+import img3 from '../../images/svg-3.svg'
+
+export const homeObjOne  = {
+    id:"about",
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topline:'Premium Bank',
+    headline:"Unlimited Transaction with zero fees",
+    description:"Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees",
+    buttonLabel:"Get Stared",
+    imgStart:false,
+    imgUrl:img1,
+    alt:'Car',
+    dark:true,
+    primary:true,
+    dartText:false,
+}
+
+
+export const homeObjTwo = {
+    id:"discover",
+    lightBg:true,
+    lightText:false,
+    lightTextDesc:false,
+    topline:'Unlimited Access',
+    headline:"Login to your account at any time",
+    description:"we have you covered no matter where you are located.All you need is internet connection and phone or computer.",
+    buttonLabel:"Learn More",
+    imgStart:true,
+    imgUrl:img2,
+    alt:'Car',
+    dark:false,
+    primary:false,
+    darkText:true,
+}
+
+export const homeObjThree  = {
+    id:"signup",
+    lightBg:true,
+    lightText:false,
+    lightTextDesc:false,
+    topline:'Join our Team',
+    headline:"Creating an account is extremly easy.",
+    description:"Get everything setup and ready under 10 minutes.All you need to do is add your information and you're ready to go.",
+    buttonLabel:"Start Now",
+    imgStart:false,
+    imgUrl:img3,
+    alt:'Car',
+    dark:false,
+    primary:false,
+    darkText:true,
+}
--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,8 +1,56 @@
 import React from 'react'
+import { Button } from '../ButtonElement';
+import {
+  Column1,
+  InfoContainer,
+  InfoRow,
+  InforWrapper,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrapper,
+  Column2,
+  ImgWrap,
+  Img
+} from './InfoElements';
 
-const InfoSection = () => {
+const InfoSection = ({
+  lightBg, id, imgStart, topline, headline, lightText, description, buttonLabel, imgUrl, alt, dark, primary, darkText
+}) => {
+  console.log(imgUrl);
   return (
-    <div>InfoSection</div>
+    <>
+      <InfoContainer lightBg={lightBg} id={id}>
+        <InforWrapper>
+          <InfoRow imgStart={imgStart}>
+            <Column1>
+              <TextWrapper>
+                <TopLine>{topline}</TopLine>
+                <Heading lightText={lightText}>{headline}</Heading>
+                <Subtitle darkText={darkText}>{description}</Subtitle>
+                <BtnWrapper>
+                  <Button
+                   to="home"
+                   smooth={true}
+                   duration={500}
+                   spy={true}
+                   exact={true}
+                   offset={-80}
+                   primary={primary? 1:0}
+                   dark={dark?1:0}>{buttonLabel}</Button>
+                </BtnWrapper>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img src={imgUrl} alt={alt} />
+              </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </InforWrapper>
+      </InfoContainer>
+    </>
   )
 }
 
