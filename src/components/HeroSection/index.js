3676d63c88096ecfde467b2556010f6cda2b34f5
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-03-02 10:02:12 +0630

    fixed route error and add scroll to top on new page

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -21,7 +21,7 @@ const HeroSection = () => {
         setHover(!hover);
     }
     return (
-        <HeroContainer>
+        <HeroContainer id="home">
             <HeroBg>
                 <VideoBg
                     autoPlay loop muted src={Video} type="video/mp4" />
--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ScrollToTop from '../components/ScrollToTop'
 import Sigin from '../components/Signin'
 
 const SigninPage = () => {
   return (
     <>
+    <ScrollToTop/>
       <Sigin/>
     </>
   )
